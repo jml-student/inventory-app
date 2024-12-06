cdc70630091fa2e3f7b0a5322f518cd681865a90
jml <student741@proton.me>
2024-12-06 21:48:18 +0100

    Create css files and add some index styles

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ init()
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
