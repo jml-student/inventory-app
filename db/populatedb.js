c02aa1c38b30b90c353a4ffa676d6de96e07c0b1
jml-student <160597100+jml-student@users.noreply.github.com>
2024-12-09 10:58:04 +0100

    Update populatedb.js

--- db/populatedb.js
+++ db/populatedb.js
@@ -15,12 +15,12 @@ CREATE TABLE IF NOT EXISTS favourites (
 `
 
 const pool = new Pool({
-  //connectionString: process.env.DATABASE_URL,
-  host: process.env.HOST,
+  connectionString: process.env.URL,
+  /*host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DB,
-  port: process.env.PORT,
+  port: process.env.PORT,*/
 })
 
 async function populateDb() {
